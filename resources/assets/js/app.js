a9b626dcf80822e5aab3c67357675109ec23da54
Jose <jalopez1423@gmail.com>
2018-07-11 20:39:00 -0700

    finished tutorial

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,24 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NoteApp from './components/NoteApp.vue';
+import routes from './routes';
+import notesStore from './store/notesStore';
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+Vue.use(VueRouter);
 
-require('./bootstrap');
+const router = new VueRouter({
+    routes
+});
 
-window.Vue = require('vue');
+window.events = new Vue();
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+window.flash = function(message, type = 'success') {
+    window.events.$emit('flash', message, type);
+};
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
+new Vue({
+    el: '#app',
+    render: h => h(NoteApp),
+    router,
+    store: notesStore,
 });
---
+++ resources/assets/js/routes.js
@@ -0,0 +1,14 @@
+import CreateNote from './components/CreateNote.vue'
+import List from './componenets/List.vue'
+import FavouriteList from './components/FavouriteList.vue'
+
+const routes = [
+    {path: '/', component: List},
+    {path: '/create', component: CreateNote},
+    {path: '/list', component: List},
+    {path: '/favourite-list', component: FavouriteList}
+    {path: '*', redirect: '/list'},
+];
+
+export default routes;
+
